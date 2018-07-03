7d691e10cb1a39f0fc856678a11796c6d141a977
realslimdjd <jamdel72@gmail.com>
2018-07-03 02:20:21 -0500

    proper attribution

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+'''
+Many thanks to https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iii-web-forms
+without which I would not have been able to complete this project (in Python)
+'''
 from flask import Flask
 
 app = Flask(__name__)
